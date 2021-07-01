a0fd67cef0974ed379bba1f96c3993bc58dc8d66
Samyak Jain <samyak65400@gmail.com>
2021-07-01 16:53:22 +0530

    exp 3 added

---
+++ 3a.py
@@ -0,0 +1,4 @@
+# Write a program for checking whether the given number is an even number or not.
+
+
+print('Even:', int(input('\nnum: ')) % 2 == 0)
---
+++ 3b.py
@@ -0,0 +1,5 @@
+# Using a for loop, write a program that prints out the decimal equivalents of 1/2, 1/3, 1/4, . . . , 1/10.
+
+
+print()
+[print(1/i) for i in range(2, 11)]
